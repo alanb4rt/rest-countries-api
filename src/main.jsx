55de4e2f9317c20484a39cdf47bc2e98668b7d30
alanb4rt <al4n.b4rt@gmail.com>
2024-08-09 13:50:44 +0200

    create homepage route with react-router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
+import ErrorPage from "./pages/error-page.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: (
+        <>
+          <App />
+        </>
+      ),
+      errorElement: <ErrorPage />,
+    },
+  ],
+  {
+    basename: "/rest-countries-api/",
+  }
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/pages/error-page.jsx
@@ -0,0 +1,20 @@
+import { useRouteError } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+export default function ErrorPage() {
+  const error = useRouteError();
+  console.error(error);
+
+  return (
+    <div id="error-page">
+      <div className="min-h-screen grid place-content-center text-center gap-4">
+        <h1 className="text-4xl font-bold">Oops!</h1>
+        <p>Sorry, an unexpected error has occurred.</p>
+        <p>
+          <i>{error.statusText || error.message}</i>
+        </p>
+        <Link to="/">Return to Homepage</Link>
+      </div>
+    </div>
+  );
+}
