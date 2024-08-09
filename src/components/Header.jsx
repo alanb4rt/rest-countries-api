17f938164bca053381ab81361f2afe2071166e72
alanb4rt <al4n.b4rt@gmail.com>
2024-08-09 12:58:19 +0200

    add dark mode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
-export default function Header() {
+export default function Header(props) {
+  const { handleDarkMode } = props;
   return (
     <>
-      <header className="bg-white w-full p-4 shadow">
+      <header className="w-full p-4 shadow">
         <div className="container mx-auto flex justify-between items-center gap-4">
           <h1 className="text-2xl font-black">Where in the world?</h1>
-          <button>Dark Mode</button>
+          <button onClick={handleDarkMode}>Dark Mode</button>
         </div>
       </header>
     </>
